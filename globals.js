e2bed283fce1df566d4cf16be3f4e97da201e8d4
Nicole Fisher <ncspence4@gmail.com>
2015-11-17 15:28:37 -0800

    empty

--- globals.js
+++
@@ -1,8 +0,0 @@
-var MIN_BOARD_SIZE = 3;
-var MAX_BOARD_SIZE = 10;
-
-var EMPTY_IMAGE_PATH = "images/st.jpg";
-var X_IMAGE_PATH = "images/x.jpg";
-var O_IMAGE_PATH = "images/o.jpg";
-
-var game = null;
